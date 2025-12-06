ca5767af916399595395252a1a6ccd809cb5fbc5
chicken8282 <117962059+chicken8282@users.noreply.github.com>
2025-12-06 13:16:50 +0900

    Title: 비교에 따른 연산, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251206/비교에 따른 연산/operation-based-on-comparison.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        StringTokenizer stk = new StringTokenizer(sc.nextLine());
+
+        int a = Integer.parseInt(stk.nextToken());
+        int b = Integer.parseInt(stk.nextToken());
+        
+        int c = (a > b) ? a*b : b / a;
+
+        System.out.println(c);
+    }
+}
